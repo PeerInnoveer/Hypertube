76b238caca9111770ef18b236541b68dd7b05c18
Pierre VAN RENSBURG <pvan-ren@c1r2s1.wethinkcode.co.za>
2019-02-18 15:46:02 +0200

    Just getting started

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import flask
+from flask import Flask, render_template
 
-app = Flask(__name___)
+app = Flask(__name__)
 
 @app.route('/')
 def route():
-    return 'INDEX'
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
